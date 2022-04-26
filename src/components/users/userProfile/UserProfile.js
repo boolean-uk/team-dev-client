afe30a1cf6801fd53072d5d4248ffaa0d18d1631
willyc <willcowendrums@gmail.com>
2022-04-26 14:28:25 +0100

    user profile route added

--- src/components/users/userProfile/UserProfile.js
+++ src/components/users/userProfile/UserProfile.js
@@ -1,12 +1,43 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import client from "../../../utils/client";
+import { useParams } from "react-router-dom";
 
-export default function UserProfile() {
+const UserProfile = () => {
+  const initialProfileState = {
+    first_name: "",
+    last_name: "",
+    email: "",
+    biography: "",
+    github_url: "",
+  };
+  const [profile, setProfile] = useState(initialProfileState);
+  const { id } = useParams();
+
+  useEffect(() => {
+      foundProfile()
+  }, [])
+
+  console.log(id);
+
+  const foundProfile = () => {
+    client.get(`/user/${id}`)
+    .then(res => {
+        setProfile(res.data.data.user)
+    })
+    .catch(err => console.log(err.response))
+  };
+
   return (
     <div>
-      <h2>First Name Last Name</h2>
-      <p>Email</p>
-      <p>Bio</p>
-      <p>Github Link</p>
+      <h2>
+        {profile.first_name} {profile.last_name}
+      </h2>
+      <p>{profile.email}</p>
+      <p>{profile.biography}</p>
+      <p>{profile.github_url}</p>
     </div>
-  )
-}
+  );
+};
+
+export default UserProfile;
