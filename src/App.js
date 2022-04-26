3c73dfe1cfdf426d9b8c124a764a2e60b7ec5d78
Safia <safia.ibrahim2121@gmail.com>
2022-04-26 17:43:23 +0100

    Added route for addCohort plus file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import './App.css';
-import LoginPage from './components/users/login/LoginPage'
-import RegistrationPage from './components/users/registration/RegistrationPage'
-import PostsPage from './components/posts/PostsPage'
+import "./App.css";
+import LoginPage from "./components/users/login/LoginPage";
+import RegistrationPage from "./components/users/registration/RegistrationPage";
+import PostsPage from "./components/posts/PostsPage";
 
-import { Routes, Route, Navigate, Outlet } from "react-router-dom"
+import { Routes, Route, Navigate, Outlet } from "react-router-dom";
+import AddCohort from "./components/addCohort/addCohort";
 
 function App() {
-
   return (
     <div className="App">
       <h1>Cohort Manager 2.0</h1>
@@ -15,6 +15,7 @@ function App() {
         <Route path="/" element={<LoginPage />} />
         <Route path="/signup" element={<RegistrationPage />} />
         <Route element={<AuthenticateUser />}>
+          <Route path="/addCohort" element={<AddCohort />} />
           <Route path="/posts" element={<PostsPage />} />
         </Route>
       </Routes>
@@ -23,13 +24,13 @@ function App() {
 }
 
 function isLoggedIn() {
-  const loadedToken = localStorage.getItem("token")
-  return !(loadedToken === '')
+  const loadedToken = localStorage.getItem("token");
+  return !(loadedToken === "");
 }
 
 export default App;
 
-const AuthenticateUser = ({children, redirectPath='/' }) => {
+const AuthenticateUser = ({ children, redirectPath = "/" }) => {
   if (!isLoggedIn()) {
     return <Navigate to={redirectPath} replace />;
   }
