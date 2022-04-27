e8664006c7c90b0d1c931162c637d5efd037e3b0
Safia <safia.ibrahim2121@gmail.com>
2022-04-27 17:18:29 +0100

    added handle to addCohort handle

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,42 +1,64 @@
-import { Box } from '@mui/system';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { Stack } from '@mui/material';
-import InputBase from '@mui/material/InputBase';
+import { Box } from "@mui/system";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { Stack } from "@mui/material";
+import InputBase from "@mui/material/InputBase";
 
 const Header = ({ companyName }) => {
+  const addCohortHandle = () => {
+    const options = {
+      method: "POST",
+      headers: {
+        authorization:
+          "Bearer " + localStorage.getItem(process.env.REACT_APP_USER_TOKEN),
+      },
+    };
+    fetch(process.env.REACT_APP_API_URL + "/cohort", options)
+      .then((res) => res.json())
+      .then((json) => console.log(json));
+  };
   return (
     <>
       <Box
         sx={{
-          display: 'flex',
-          backgroundColor: 'grey',
-          justifyContent: 'space-between',
-          alignContent: 'center',
-          width: '100vw',
-          padding: '1em'
+          display: "flex",
+          backgroundColor: "grey",
+          justifyContent: "space-between",
+          alignContent: "center",
+          width: "100vw",
+          padding: "1em",
         }}
       >
-
         <Box>
           <Typography>
             <p>{companyName}</p>
           </Typography>
         </Box>
 
-        <Box sx={{ display: 'flex', justifyContent: 'center', alignContent: 'center' }}>
-          <Box sx={{backgroundColor: 'white'}}>
-          <InputBase placeholder='Search…' inputProps={{ 'aria-label': 'search' }} />
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignContent: "center",
+          }}
+        >
+          <Box sx={{ backgroundColor: "white" }}>
+            <InputBase
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
+            />
           </Box>
           <Box>
-            <Button variant='contained'>Search User</Button>
+            <Button variant="contained">Search User</Button>
           </Box>
         </Box>
 
         <Box>
-          <Stack spacing={2} direction='row'>
-          <Button variant='contained'>Add Cohort</Button>
-          <Button variant='contained'>Logout</Button>
+          <Stack spacing={2} direction="row">
+            <Button variant="contained" onClick={addCohortHandle}>
+              Add Cohort
+            </Button>
+            <Button variant="contained">Logout</Button>
           </Stack>
         </Box>
       </Box>
