5f68b3c2feb21b1b2c3f2984e729e9ce3704d9e2
Jason Kundi <94011909+JasonKundi@users.noreply.github.com>
2022-04-27 16:40:11 +0100

    edit button created and linked to form page

--- src/components/users/userProfile/UserProfile.js
+++ src/components/users/userProfile/UserProfile.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import { Route, Routes } from "react-router";
 import { useState, useEffect } from "react";
 import client from "../../../utils/client";
 import { useParams } from "react-router-dom";
@@ -26,6 +28,7 @@ const UserProfile = () => {
     .catch(err => console.log(err.response))
   };
 
+
   return (
     <div>
       <h1>User Profile</h1>
@@ -35,8 +38,11 @@ const UserProfile = () => {
       <p>Email: {profile.email}</p>
       <p>Bio: {profile.biography}</p>
       <p>Github: {profile.github_url}</p>
+      <Link id='edit-profile-button' to={`/user/edit/${id}`}> Edit Profile
+      </Link>
     </div>
   );
 };
+
 
 export default UserProfile;
