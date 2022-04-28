7dd2f105e8097e33d0b47fd367a0e59edc013af3
Elvis Onobhayedo <elvisonob@yahoo.com>
2022-04-28 15:44:41 +0100

    merged

--- src/components/posts/PostsPage.js
+++ src/components/posts/PostsPage.js
@@ -1,17 +1,19 @@
-import { useState, useEffect } from 'react';
-import PostForm from './PostForm';
-import client from '../../utils/client';
-import './style.css';
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import PostForm from "./PostForm";
+import client from "../../utils/client";
+import "./style.css";
 
-import Header from '../Header/Header';
-
-const PostsPage = ({role}) => {
-  const [post, setPost] = useState({ content: '' });
-  const [postResponse, setPostResponse] = useState('');
+const PostsPage = () => {
+  const [post, setPost] = useState({ content: "" });
+  const [postResponse, setPostResponse] = useState("");
   const [posts, setPosts] = useState([]);
+  const [commentResponse, setcommentResponse] = useState({})
+  const [comment, setComment] = useState("")
+  let navigate = useNavigate();
 
   useEffect(() => {
-    client.get('/posts').then((res) => setPosts(res.data.data.posts));
+    client.get("/posts").then((res) => setPosts(res.data.data.posts));
   }, []);
 
   const createPost = async (event) => {
@@ -33,21 +35,59 @@ const PostsPage = ({role}) => {
     });
   };
 
+  const signOut = (event) => {
+    event.preventDefault();
+    localStorage.setItem(process.env.REACT_APP_USER_TOKEN, "");
+    navigate("../", { replace: true });
+  };
+
+  const createComment = async (event) => {
+    console.log(event.target)
+    event.preventDefault();
+    client
+      .post("/post/comment", comment)
+      .then((res) => {
+        setcommentResponse(res.data)
+      })
+      .catch((data) => {
+        console.log(data);
+      });
+  };
+
+  const handleComment = (event) => {
+    console.log(event.target)
+    event.preventDefault();
+    const { value, name } = event.target;
+    setComment({
+      ...comment,
+      [name]: value,
+    });
+  };
   return (
-    <>
-      <Header role={role}/>
-      <section className='posts-section'>
-        <p>Status: {postResponse.status}</p>
-        <PostForm handleSubmit={createPost} handleChange={handleChange} />
-        <ul className='posts-list'>
-          {posts.map((post, index) => (
-            <li key={index} className='post-item'>
-              {post.content}
-            </li>
-          ))}
-        </ul>
-      </section>
-    </>
+    <section className='posts-section'>
+      <button id='user-signout-button' onClick={signOut}>
+        sign out
+      </button>
+      <p>Status: {postResponse.status}</p>
+      <PostForm handleSubmit={createPost} handleChange={handleChange} />
+      <ul className='posts-list'>
+        {posts.map((post, index) => (
+          <li key={index} className='post-item'>
+            {post.content}
+            <div className="comments-section">
+              <form onSubmit={createComment} >
+                <input type='text' className="post__comment" onChange={handleComment} name="comment" label="New Comment" variant="outlined"/>
+                <button type="button" className="comment-button" >Comment</button>
+              </form>
+              <ul className="comments-list">
+               {commentResponse.data && 
+                <li className="comment-item">{commentResponse.data.comment.content} </li >}
+              </ul>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
   );
 };
 
