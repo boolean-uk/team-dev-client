5436e7671cec9e11de01436710e7aef0915662b7
Paul Smart <pdsmart6@gmail.com>
2022-04-29 14:50:58 +0100

    updated helperfunction

--- src/components/posts/helperfunctions.js
+++ src/components/posts/helperfunctions.js
@@ -1,22 +1,9 @@
-import { differenceInHours, differenceInDays } from 'date-fns'
+import {formatDistance} from 'date-fns'
 
 function dateTimetoRelativeTime(datetime){
     let postTime = new Date(datetime)
     let currentTime = new Date;
-    const hoursago = differenceInHours(currentTime,postTime)
-    const daysago = differenceInDays(currentTime,postTime)
-    if (daysago == 0)
-    {
-        if (hoursago == 0) return 'recent'
-        else {
-            
-            if (hoursago == 1) return `1 hour ago`
-            else return `${hoursago} hours ago`
-        }
-    }
-    
-    if (daysago == 1) return `1 day ago`
-    else return `${differenceInDays(currentTime,postTime)} days ago`
+    return formatDistance(postTime, currentTime, {addSuffix: true})
 }
 
 export default dateTimetoRelativeTime
