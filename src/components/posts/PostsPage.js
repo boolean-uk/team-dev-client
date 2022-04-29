38e6a4db8ee440a487bb631c758ff6d5ada83934
Paul Smart <pdsmart6@gmail.com>
2022-04-29 14:46:01 +0100

    fixed formatting

--- src/components/posts/PostsPage.js
+++ src/components/posts/PostsPage.js
@@ -1,30 +1,28 @@
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import PostForm from './PostForm';
-import client from '../../utils/client';
-import './style.css';
-import { Box, Stack } from '@mui/material';
-import Header from '../Header/Header';
-import dateTimetoRelativeTime from './helperfunctions'
-
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import PostForm from "./PostForm";
+import client from "../../utils/client";
+import "./style.css";
+import { Box, Stack } from "@mui/material";
+import Header from "../Header/Header";
+import dateTimetoRelativeTime from "./helperfunctions";
 
 const PostsPage = () => {
-  const [post, setPost] = useState({ content: '' });
-  const [postResponse, setPostResponse] = useState('');
+  const [post, setPost] = useState({ content: "" });
+  const [postResponse, setPostResponse] = useState("");
   const [posts, setPosts] = useState([]);
   let navigate = useNavigate();
 
   useEffect(() => {
-    client.get('/posts').then((res) => setPosts(res.data.data.posts));
+    client.get("/posts").then((res) => setPosts(res.data.data.posts));
   }, []);
 
   const createPost = async (event) => {
     event.preventDefault();
     client
-      .post('/post', post)
+      .post("/post", post)
       .then((res) => setPostResponse(res.data))
-      .catch((data) => {
-      });
+      .catch((data) => {});
   };
 
   const handleChange = (event) => {
@@ -38,29 +36,31 @@ const PostsPage = () => {
 
   const signOut = (event) => {
     event.preventDefault();
-    localStorage.setItem(process.env.REACT_APP_USER_TOKEN, '');
-    navigate('../', { replace: true });
+    localStorage.setItem(process.env.REACT_APP_USER_TOKEN, "");
+    navigate("../", { replace: true });
   };
 
   return (
     <>
       <Header companyName={`Cohort Manager 2.0`} />
-      <section className='posts-section'>
-        <button id='user-signout-button' onClick={signOut}>
+      <section className="posts-section">
+        <button id="user-signout-button" onClick={signOut}>
           sign out
         </button>
         <p>Status: {postResponse.status}</p>
         <PostForm handleSubmit={createPost} handleChange={handleChange} />
-        <ul className='posts-list'>
+        <ul className="posts-list">
           {posts.map((post, index) => (
-            <li key={index} className='post-item'>
-            <Box>
-            <div className='post-content'>{post.content}</div>
-            <Stack spacing={2} direction='row'>
-            <Box variant='contained'>{`${post.user.profile.firstName} ${post.user.profile.lastName}`}</Box>
-            <Box variant='contained'>{dateTimetoRelativeTime(post.createdAt)}</Box>
-            </Stack>
-            </Box>
+            <li key={index} className="post-item">
+              <Box>
+                <div className="post-content">{post.content}</div>
+                <Stack spacing={2} direction="row">
+                  <Box variant="contained">{`${post.user.profile.firstName} ${post.user.profile.lastName}`}</Box>
+                  <Box variant="contained">
+                    {dateTimetoRelativeTime(post.createdAt)}
+                  </Box>
+                </Stack>
+              </Box>
             </li>
           ))}
         </ul>
