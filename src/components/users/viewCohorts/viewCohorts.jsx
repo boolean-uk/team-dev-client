39d0e88d83e942800e8e0a309f36fcfbc5e7b059
Daniel Mc <danielmc122333@icloud.com>
2022-05-03 13:49:42 +0100

    isLoggedIn updates

---
+++ src/components/users/viewCohorts/viewCohorts.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+export default function viewCohorts() {
+// get a list of students + with button 
+// add button left - student right 
+
+
+
+  return (
+    <div>viewCohorts</div>
+  )
+}
