bfef6038df8362efb9c107af5f572232f44b1cde
Jason Kundi <94011909+JasonKundi@users.noreply.github.com>
2022-05-03 15:10:07 +0100

    resolved conflicts in app

--- src/App.js
+++ src/App.js
@@ -4,15 +4,7 @@ import RegistrationPage from "./components/users/registration/RegistrationPage";
 import PostsPage from "./components/posts/PostsPage";
 import EditUser from "./components/users/userProfile/EditUserProfile";
 import ProfilePage from "./components/users/userProfile/UserProfile";
-<<<<<<< HEAD
 import { useState } from "react";
-=======
-<<<<<<< HEAD
-
-=======
-import { useState } from "react";
->>>>>>> origin
->>>>>>> ff0e1fc0ba23169600ab3850eac966cd08a7eab0
 import { Routes, Route, Navigate, Outlet } from "react-router-dom";
 
 function App() {
@@ -24,7 +16,7 @@ function App() {
         <Route path="/login" element={<LoginPage setRole={setRole} />} />
         <Route path="/signup" element={<RegistrationPage />} />
         <Route element={<AuthenticateUser />}>
-          <Route path="/" element={<PostsPage role={role}/>} />
+          <Route path="/" element={<PostsPage role={role} />} />
           <Route path="/user/:id" element={<ProfilePage />} />
           <Route path="/user/edit/:id" element={<EditUser />} />
         </Route>
