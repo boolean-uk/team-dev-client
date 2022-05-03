cd5b9b1f9f355276b6873d6b92a5d7e29916ae98
Safia <safia.ibrahim2121@gmail.com>
2022-05-03 16:12:29 +0100

    added cohort page

---
+++ src/components/cohort/CohortList.js
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+import client from "../../utils/client";
+import Header from "../Header/Header";
+import "./style.css";
+
+function CohortList() {
+  const [cohort, setCohort] = useState([]);
+  return;
+  <>
+    <Header role={role} />
+  </>;
+}
+
+export default CohortList;
