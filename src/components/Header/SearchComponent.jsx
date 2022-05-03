64b92c789ca20b729a9682a3ea3c02c1b6331e2f
Ben Corbin <ben@corbin.digital>
2022-05-03 15:40:40 +0100

    Todo list added to search component

--- src/components/Header/SearchComponent.jsx
+++ src/components/Header/SearchComponent.jsx
@@ -3,6 +3,21 @@ import Button from "@mui/material/Button"
 import InputBase from "@mui/material/InputBase"
 import { useEffect, useState } from "react"
 
+
+/* 
+TO DO:-
+
+* Check get route is working using TC
+* Work out where the component is going to live 
+* Write onSubmit function that sends a GET request to the server with
+  the search term in the body 
+* Return the search results into state array
+* Send state up to App
+* Send state as props from App to Seach Component / Search Page for rendering
+
+
+*/
+
 export function SearchComponent({handleSubmit}) {
   return (
     <Box
