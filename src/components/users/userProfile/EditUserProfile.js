b71399c73c93c76e50217d2b3576b6d597ae3cd6
Jason Kundi <94011909+JasonKundi@users.noreply.github.com>
2022-05-04 13:20:38 +0100

    Final tweaks on put made

--- src/components/users/userProfile/EditUserProfile.js
+++ src/components/users/userProfile/EditUserProfile.js
@@ -1,21 +1,23 @@
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import React from 'react';
-import { useState, useEffect } from 'react';
-import client from '../../../utils/client';
-import { useParams } from 'react-router-dom';
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import React from "react";
+import { useState, useEffect } from "react";
+import client from "../../../utils/client";
+import { useParams } from "react-router-dom";
 
 const UserForm = () => {
-
   const initialFormData = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    password: '',
-    biography: '',
-    githubUrl: ''
-  }
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    biography: "",
+    githubUrl: "",
+  };
+
   const [profile, setProfile] = useState(initialFormData);
+  const [editUserResponse, setEditUserResponse] = useState("");
+
   const { id } = useParams();
 
   useEffect(() => {
@@ -25,37 +27,33 @@ const UserForm = () => {
         setProfile(res.data.data.user);
       })
       .catch((err) => console.log(err.response));
-  }, [])
-  
+  }, []);
+
   const handleChange = (event) => {
-    event.preventDefault()
-    const { value, name } = event.target
+    event.preventDefault();
+    const { value, name } = event.target;
 
     setProfile({
       ...profile,
       [name]: value,
     });
-  }
+  };
 
   const onSubmit = (e) => {
     e.preventDefault();
-    const options = {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        firstName: profile.firstName,
-        lastName: profile.lastName,
-        email: profile.email,
-        password: profile.password,
-        biography: profile.biography,
-        githubUrl: profile.githubUrl,
-      }),
+    const putBody = {
+      firstName: profile.firstName,
+      lastName: profile.lastName,
+      email: profile.email,
+      password: profile.password,
+      bio: profile.biography,
+      githubUrl: profile.githubUrl,
     };
-    fetch(`/user/${id}`, options)
+    client
+      .put("/user", putBody)
+      .then((res) => res.json)
       .then((res) => {
-        setProfile(res.data.data.user);
+        setEditUserResponse(res.data);
       })
       .catch((err) => console.log(err.response));
   };
@@ -63,53 +61,37 @@ const UserForm = () => {
   return (
     <div>
       <h1> Edit Profile Details </h1>
-      <form onSubmit={onSubmit} className='user-form'>
+      <form onSubmit={onSubmit} className="user-form">
         <TextField
-          className='user-form-input'
+          className="user-form-input"
           value={profile.firstName}
-          variant='outlined'
-          name='firstName'
+          variant="outlined"
+          name="firstName"
           onChange={handleChange}
         />
         <TextField
-          className='user-form-input'
+          className="user-form-input"
           value={profile.lastName}
-          variant='outlined'
-          name='lastName'
+          variant="outlined"
+          name="lastName"
           onChange={handleChange}
         />
         <TextField
-          className='user-form-input'
-          type='email'
-          value={profile.email}
-          variant='outlined'
-          name='email'
-          onChange={handleChange}
-        />
-        <TextField
-          className='user-form-input'
-          type='password'
-          label='Password'
-          variant='outlined'
-          name='password'
-          onChange={handleChange}
-        />
-        <TextField
-          className='user-form-input'
+          className="user-form-input"
           value={profile.biography}
-          variant='outlined'
-          name='biography'
+          variant="outlined"
+          name="biography"
           onChange={handleChange}
         />
         <TextField
-          className='user-form-input'
-          type='url'
+          className="user-form-input"
+          type="url"
           value={profile.githubUrl}
-          variant='outlined'
-          name='githubUrl'
+          variant="outlined"
+          name="githubUrl"
           onChange={handleChange}
         />
-        <Button id='user-submit-button' type='submit' variant='contained'>
+        <Button id="user-submit-button" type="submit" variant="contained">
           Submit
         </Button>
       </form>
