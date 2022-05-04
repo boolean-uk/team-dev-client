1b6a06c560db5ffa5f305ea88962c644eb02fad6
willyc <willcowendrums@gmail.com>
2022-05-04 18:11:07 +0100

    progress on default profile img

--- src/components/users/userProfile/UserProfile.js
+++ src/components/users/userProfile/UserProfile.js
@@ -4,26 +4,36 @@ import client from '../../../utils/client';
 import { useParams } from 'react-router-dom';
 import Header from '../../Header/Header';
 
-
 const UserProfile = () => {
-
   const [profile, setProfile] = useState('');
+  const [profileImg, setProfileImg] = useState('');
   const { id } = useParams();
-  let role = profile.role
+  let role = profile.role;
 
   useEffect(() => {
-      client
-        .get(`/user/${id}`)
-        .then((res) => {
-          setProfile(res.data.data.user);
-        })
-        .catch((err) => console.log(err.response));
+    handleProfile();
   }, [id]);
+
+  const handleProfile = () => {
+     client
+      .get(`/user/${id}`)
+      .then((res) => {
+        setProfile(res.data.data.user);
+        const firstName = res.data.data.user.firstName
+        const lastName = res.data.data.user.lastName
+        fetch(
+          `https://ui-avatars.com/api/?name=${firstName}+${lastName}`
+        )
+        .then(res=> { setProfileImg(res.url)} )
+      })
+      .catch((err) => console.log(err.response));
+  };
 
   return (
     <div>
-      <Header role={role}/>
+      <Header role={role} />
       <h1>User Profile</h1>
+      <img src={profileImg} alt="avatar" width="50" height="50" />
       <h2>
         {profile.firstName} {profile.lastName}
       </h2>
