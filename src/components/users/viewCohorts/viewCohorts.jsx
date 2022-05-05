02eef2ab2cbaca8beb33fe967b15103f2f527c97
Kiran Gurung <kg820532@gmail.com>
2022-05-05 16:15:18 +0100

    object key typ correction

--- src/components/users/viewCohorts/viewCohorts.jsx
+++ src/components/users/viewCohorts/viewCohorts.jsx
@@ -19,12 +19,11 @@ export default function ViewCohort() {
 function addStudent(studentId) {
     const options = {
       body: JSON.stringify({
-        studentId: studentId
+        student_Id: studentId
       })
     }
   client.patch(`/user/student/cohort/${id}`, options)
   .then((res) => res.json())
-  .then((data) => console.log(data))
   .catch((err) => console.log(err.response));
 }
 
@@ -45,11 +44,7 @@ function addStudent(studentId) {
 
               <div className="add-student-card" key={key}>
                 <div className="add-student">
-<<<<<<< HEAD
                   {student.user.firstName} {student.user.lastName}
-=======
-                  {student.profile.firstName} {student.profile.lastName}
->>>>>>> 149372d2a8bb8f4e574ab78404bb7246f2e33aca
                 </div>
                 <button onClick={() => {addStudent(student.user.id)}} value={student.id}>
                   Add
