e1f644f9edce9f093147767f6436cc2b9462f8c2
usamalodhi <43435750+usamalodhi@users.noreply.github.com>
2022-05-05 06:13:21 +0100

    Add styles to searched users list

--- src/components/search/SearchPage.js
+++ src/components/search/SearchPage.js
@@ -1,41 +1,46 @@
-import React from "react"
-import { useEffect, useState } from "react"
-import client from "../../utils/client"
-import Box from "@mui/material/Box"
-import List from "@mui/material/List"
-import ListItem from "@mui/material/ListItem"
-import ListItemButton from "@mui/material/ListItemButton"
-// import ListItemIcon from "@mui/material/ListItemIcon"
-import ListItemText from "@mui/material/ListItemText"
+import React from 'react';
+import { useEffect, useState } from 'react';
+import client from '../../utils/client';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
 
 export default function SearchPage({ searchInput }) {
-  const [searchResults, setSearchResults] = useState([])
+  const [searchResults, setSearchResults] = useState([]);
 
   useEffect(() => {
     if (!searchInput) {
-      return
+      return;
     }
     client
       .get(`/user?first_name=${searchInput}`)
       .then((res) => {
-        console.log("go results:", res)
-        setSearchResults(res.data.data.users)
+        console.log('go results:', res);
+        setSearchResults(res.data.data.users);
       })
-      .catch((err) => console.log("Error", err))
-    console.log("Search page", searchInput)
-  }, [searchInput])
+      .catch((err) => console.log('Error', err));
+    console.log('Search page', searchInput);
+  }, [searchInput]);
 
   return (
     <>
-    <Box  sx={{ display: "flex", justifyContent: "center", alignContent: "center" }}>
+      <Box sx={{ display: 'flex', justifyContent: 'center', alignContent: 'center' }}>
         <List>
-            {searchResults.map((user, index) => (
-            <ListItem key={index}>
-                <p>{`${user.firstName} ${user.lastName}`}</p>
-            </ListItem>
-         ))}
-      </List>
-    </Box>
+          {searchResults.map((user, index) => (
+            <>
+              <ListItem disablePadding>
+                <ListItemButton key={index} component='a' href='#simple-list'>
+                  <ListItemText primary={`${user.firstName} ${user.lastName}`} />
+                </ListItemButton>
+              </ListItem>
+              <Divider />
+            </>
+          ))}
+        </List>
+      </Box>
     </>
   );
-};
+}
