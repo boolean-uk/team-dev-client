6efb7721b16af3037a1dfdd16b8a1f3cbd9d1390
marianphillips <79763160+marianphillips@users.noreply.github.com>
2022-05-05 09:24:56 +0100

    updated

--- src/components/users/login/LoginPage.js
+++ src/components/users/login/LoginPage.js
@@ -1,45 +1,45 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import UserForm from './UserForm';
-import userBlankData from '../utils/userHelpers';
-import client from '../../../utils/client';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import UserForm from "./UserForm";
+import userBlankData from "../utils/userHelpers";
+import client from "../../../utils/client";
+import { useNavigate } from "react-router-dom";
 
-const LoginPage = (props) => {
-  const { setRole } = props;
+const LoginPage = () => {
   const [user, setUser] = useState(userBlankData());
-  
-  const [loginResponse, setLoginResponse] = useState({ data: { token: '', user: {} } });
+
+  const [loginResponse, setLoginResponse] = useState({
+    data: { token: "", user: {} },
+  });
   const [loginError, setLoginError] = useState(false);
   let navigate = useNavigate();
 
   useEffect(() => {
     const loadedToken =
-      localStorage.getItem(process.env.REACT_APP_USER_TOKEN) || '';
+      localStorage.getItem(process.env.REACT_APP_USER_TOKEN) || "";
     setLoginResponse({ data: { token: loadedToken } });
   }, []);
 
   const loginUser = (event) => {
     event.preventDefault();
     client
-      .post('/login', user)
+      .post("/login", user)
       .then((res) => {
-        setRole(res.data.data.user.role);
         localStorage.setItem(
           process.env.REACT_APP_USER_TOKEN,
-          res.data.data.token, 
+          res.data.data.token
         );
-        localStorage.setItem("role", res.data.data.user.role)
+        localStorage.setItem("role", res.data.data.user.role);
         const userId = res.data.data.user.id;
-        localStorage.setItem('userId', userId);
+        localStorage.setItem("userId", userId);
         setLoginResponse(res.data);
-        navigate('../', { replace: true });
+        navigate("../", { replace: true });
       })
       .catch((err) => {
-        console.log(err.response)
-        const errorMessage = err.response.data.data.email
-        setLoginError(errorMessage)
+        console.log(err.response);
+        const errorMessage = err.response.data.data.email;
+        setLoginError(errorMessage);
       });
   };
 
@@ -66,7 +66,11 @@ const LoginPage = (props) => {
       </Link>
       <h1>Login</h1>
       <p>Status: {loginResponse.status}</p>
-      <UserForm handleChange={handleChange} handleSubmit={loginUser} loginError={loginError} />
+      <UserForm
+        handleChange={handleChange}
+        handleSubmit={loginUser}
+        loginError={loginError}
+      />
     </div>
   );
 };
