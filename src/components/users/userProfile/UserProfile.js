f0534cf3bf60ed0340e0455f3494b2a9e4669dbe
willyc <willcowendrums@gmail.com>
2022-05-05 11:53:20 +0100

    added default profile img on user profile

--- src/components/users/userProfile/UserProfile.js
+++ src/components/users/userProfile/UserProfile.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import client from '../../../utils/client';
 import { useParams } from 'react-router-dom';
 import Header from '../../Header/Header';
+import './style.css'
 
 const UserProfile = () => {
   const [profile, setProfile] = useState('');
@@ -33,13 +34,19 @@ const UserProfile = () => {
     <div>
       <Header role={role} />
       <h1>User Profile</h1>
-      <img src={profileImg} alt="avatar" width="50" height="50" />
+      <div className="profile">
+      <div className="profile-img">
+      <img src={profileImg} alt="avatar" width="60" height="60" />
+      </div>
+      <div className="profile-info">
       <h2>
         {profile.firstName} {profile.lastName}
       </h2>
       <p>Email: {profile.email}</p>
       <p>Bio: {profile.biography}</p>
       <p>Github: {profile.githubUrl}</p>
+      </div>
+      </div>
     </div>
   );
 };
