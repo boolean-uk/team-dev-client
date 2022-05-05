b260a5ca9f0ea2156e3b3b1909491e1b4a86eeda
Nico Picchio <nicolapicchio@gmail.com>
2022-05-05 17:29:59 +0100

    added placeholder for profile img to posts

--- src/components/posts/PostsPage.js
+++ src/components/posts/PostsPage.js
@@ -9,63 +9,69 @@ import dateTimetoRelativeTime from './helperfunctions';
 import { Link } from 'react-router-dom';
 
 const PostsPage = ({ role }) => {
-  const [post, setPost] = useState({ content: '' });
-  const [postResponse, setPostResponse] = useState('');
-  const [posts, setPosts] = useState([]);
+	const [post, setPost] = useState({ content: '' });
+	const [postResponse, setPostResponse] = useState('');
+	const [posts, setPosts] = useState([]);
+  console.log(posts)
 
-  useEffect(() => {
-    client.get('/posts').then((res) => setPosts(res.data.data.posts));
-  }, []);
+	useEffect(() => {
+		client.get('/posts').then((res) => setPosts(res.data.data.posts));
+	}, []);
 
-  const createPost = async (event) => {
-    event.preventDefault();
-    client
-      .post('/post', post)
-      .then((res) => {
-        setPostResponse(res.data);
-        setPosts((posts) => [res.data.data.post, ...posts]);
-      })
-      .catch((data) => {});
-    setPost(() => ({ content: '' }));
-  };
+	const createPost = async (event) => {
+		event.preventDefault();
+		client
+			.post('/post', post)
+			.then((res) => {
+				setPostResponse(res.data);
+				setPosts((posts) => [res.data.data.post, ...posts]);
+			})
+			.catch((data) => {});
+		setPost(() => ({ content: '' }));
+	};
 
-  const handleChange = (event) => {
-    event.preventDefault();
-    const { value, name } = event.target;
-    setPost({
-      ...post,
-      [name]: value,
-    });
-  };
+	const handleChange = (event) => {
+		event.preventDefault();
+		const { value, name } = event.target;
+		setPost({
+			...post,
+			[name]: value,
+		});
+	};
 
-  return (
-    <>
-      <Header role={role} />
-      <section className='posts-section'>
-        {postResponse.status}
-        <PostForm handleSubmit={createPost} handleChange={handleChange} inputValue={post.content} />
-        <ul className="posts-list">
-          {posts.map((post, index) => (
-            <li key={index} className='post-item'>
-              <Box>
-                <div className="post-content">{post.content}</div>
-                <Stack className="names-date" spacing={2} direction="row">
-                  <Link to={`/user/${post.user.id}`} className='post-author'>
-                    <Box className="fullname" variant='contained'>
-                      <strong>{`${post.user.profile.firstName} ${post.user.profile.lastName}`}</strong>
-                    </Box>
-                  </Link>
-                  <Box className="date-time" variant="contained">
-                    {dateTimetoRelativeTime(post.createdAt)}
-                  </Box>
-                </Stack>
-              </Box>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </>
-  );
+	return (
+		<>
+			<Header role={role} />
+			<section className='posts-section'>
+				{postResponse.status}
+				<PostForm
+					handleSubmit={createPost}
+					handleChange={handleChange}
+					inputValue={post.content}
+				/>
+				<ul className='posts-list'>
+					{posts.map((post, index) => (
+						<li key={index} className='post-item'>
+							<Box>
+								<div className='post-content'>{post.content}</div>
+								<Stack className='names-date' spacing={2} direction='row'>
+                      <img src="" alt="avatar" />
+									<Link to={`/user/${post.user.id}`} className='post-author'>
+										<Box className='fullname' variant='contained'>
+											<strong>{`${post.user.profile.firstName} ${post.user.profile.lastName}`}</strong>
+										</Box>
+									</Link>
+									<Box className='date-time' variant='contained'>
+										{dateTimetoRelativeTime(post.createdAt)}
+									</Box>
+								</Stack>
+							</Box>
+						</li>
+					))}
+				</ul>
+			</section>
+		</>
+	);
 };
 
 export default PostsPage;
