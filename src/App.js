246f44c3d96fa060a63665aaff4412e3fa6cccb9
marianphillips <79763160+marianphillips@users.noreply.github.com>
2022-05-06 17:57:04 +0100

    sorted a sneaky missing semi colon

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import PostsPage from './components/posts/PostsPage';
 import EditUser from './components/users/userProfile/EditUserProfile';
 import ProfilePage from './components/users/userProfile/UserProfile';
 import ViewCohort from './components/users/viewCohorts/viewCohorts';
-import { useState } from 'react'
+import { useState } from 'react';
 import { Routes, Route, Navigate, Outlet } from 'react-router-dom';
 import Header from './components/Header/Header';
 import SearchPage from './components/search/SearchPage';
