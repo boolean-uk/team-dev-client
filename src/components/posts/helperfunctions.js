f4f9fd1ac719c4271c0745d1b8bddf7a0179916b
Paul Smart <pdsmart6@gmail.com>
2022-05-06 16:16:43 +0100

    updated package.json, fixed linting

--- src/components/posts/helperfunctions.js
+++ src/components/posts/helperfunctions.js
@@ -1,9 +1,9 @@
-import {formatDistance} from 'date-fns'
+import {formatDistance} from 'date-fns';
 
 function dateTimetoRelativeTime(datetime){
-    let postTime = new Date(datetime)
+    let postTime = new Date(datetime);
     let currentTime = new Date();
-    return formatDistance(postTime, currentTime, {addSuffix: true})
+    return formatDistance(postTime, currentTime, {addSuffix: true});
 }
 
-export default dateTimetoRelativeTime
+export default dateTimetoRelativeTime;
