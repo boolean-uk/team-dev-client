6d2ad24c8cde72b005af29b601432bff44aa841f
Daniel Mc <danielmc122333@icloud.com>
2022-05-10 12:34:11 +0100

    added left view cohort table

--- src/components/users/viewCohorts/viewCohorts.jsx
+++ src/components/users/viewCohorts/viewCohorts.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import "./viewCohort.css";
-import Header from "../../Header/Header.jsx";
-import client from "../../../utils/client";
-import Table from "../../table/Table";
+import React from "react"
+import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
+import "./viewCohort.css"
+import Header from "../../Header/Header.jsx"
+import client from "../../../utils/client"
+import Table from "../../table/Table"
 import {
   Box,
   Button,
@@ -16,36 +16,36 @@ import {
   ListItemText,
   Avatar,
   IconButton,
-} from "@mui/material";
-import AddCircleIcon from "@mui/icons-material/AddCircle";
+} from "@mui/material"
+import AddCircleIcon from "@mui/icons-material/AddCircle"
 
 export default function ViewCohort() {
-  const { id } = useParams();
-  const [noCohort, setNoCohort] = useState([]);
-  const [cohortStudents, setCohortStudents] = useState([]);
-  const [resetStudents, setResetStudents] = useState(0);
+  const { id } = useParams()
+  const [noCohort, setNoCohort] = useState([])
+  const [cohortStudents, setCohortStudents] = useState([])
+  const [resetStudents, setResetStudents] = useState(0)
 
   useEffect(() => {
     client
-      .get(`/user/student?cohort=none`)
+      .get("/user/student?cohort=none")
       .then((res) => setNoCohort(res.data.data))
-      .catch((err) => console.log(err.response));
+      .catch((err) => console.log(err.response))
 
     client
       .get(`/user/student?cohort=${id}`)
       .then((res) => setCohortStudents(res.data.data))
-      .catch((err) => console.log(err.response));
-  }, [resetStudents]);
+      .catch((err) => console.log(err.response))
+  }, [resetStudents])
 
   function addStudent(studentId) {
-    const data = { cohort_id: id };
+    const data = { cohort_id: id }
     client
       .patch(`/user/${studentId}/cohort`, data)
       .then((res) => {
-        setResetStudents(resetStudents + 1);
-        console.log(res.data);
+        setResetStudents(resetStudents + 1)
+        console.log(res.data)
       })
-      .catch((err) => console.log(err.response));
+      .catch((err) => console.log(err.response))
   }
 
   return (
@@ -58,15 +58,22 @@ export default function ViewCohort() {
         alignItems="center"
       >
         <Box className="BigContainer">
-          <Box className="Container_cohorts">
-            <h3 className="cohort-title">Cohort {id}</h3>
-            <Table cohortStudents={cohortStudents} />
-          </Box>
+          <div className="Container_cohorts">
+            <div className="Parent-title-container">
+              <div className="cohort-title">
+                <h3>Cohort {id}</h3>{" "}
+              </div>
+              <div className="cohort-size-text"> Number of students: 0</div>
+            </div>
+            <div className="cohort-student-big-table">
+              <Table cohortStudents={cohortStudents} />
+            </div>
+          </div>
 
           <Box className="Container_addStudent">
-            <Box>
+            <div className="box-title">
               <h3>Available students</h3>
-            </Box>
+            </div>
             <Box className="add-student-container">
               <List>
                 {noCohort.map((student, key) => (
@@ -78,20 +85,24 @@ export default function ViewCohort() {
                     }}
                   >
                     {/* <ListItemAvatar> */}
-                    <Box sx={{ display: "flex" }}>
+                    <Box
+                      sx={{ display: "flex" }}
+                      className="individual-student-box"
+                    >
                       <Box>
                         <Avatar></Avatar>
                       </Box>
                       <Box>
-                        {`${student.user.firstName} ${student.user.lastName}`}
+                        {`${student.user.firstName}
+                        ${student.user.lastName}`}
                       </Box>
-                      <Box>
+                      <Box className="add-student-icon">
                         <AddCircleIcon
                           onClick={() => {
-                            addStudent(student.user.id);
+                            addStudent(student.user.id)
                           }}
                           value={student.id}
-                          color="success"
+                          color="string"
                           fontSize="large"
                         >
                           Add
@@ -104,8 +115,8 @@ export default function ViewCohort() {
                 ))}
               </List>
               {/* {noCohort.map((student, key) => (
-                <Box className="add-student-card" key={key}>
-                  <Box className="add-student">
+                <Box className='add-student-card' key={key}>
+                  <Box className='add-student'>
                     {student.user.firstName} {student.user.lastName}
                     <Button
                       onClick={() => {
@@ -123,5 +134,5 @@ export default function ViewCohort() {
         </Box>
       </Grid>
     </>
-  );
+  )
 }
