2b0f3470296f1f4d4c1127333fdfa47fe3959fa3
Paul Smart <pdsmart6@gmail.com>
2022-05-10 15:09:16 +0100

    fixed bugs with like button - on refresh + waiting for talk with server

--- src/components/posts/PostLike.js
+++ src/components/posts/PostLike.js
@@ -1,43 +1,59 @@
 import like from "./like.png"
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import client from '../../utils/client';
 import './style.css';
 
-const PostLike = ({likes=[], postId}) => {
-      
-const [divStyle, updateLike] = useState({
+const blue = `#1976d2`
+
+const PostLike = ({likes=[], postId}) => { 
+const [divStyle, updateStyle] = useState({
   backgroundColor: 'grey'
   });
 const [postLikes,updateLikes] = useState(likes)
 const [id, updateId] = useState("");
+const [talkingToServer, updateTalkStatus] = useState(false)
+
+useEffect(()=>{
+  const like = likes.find(like => String(like.userId) === localStorage.getItem('userId'))
+  if (likes.find(like => String(like.userId) === localStorage.getItem('userId'))){
+    updateStyle({backgroundColor: blue})
+    updateId(like.id)
+  }
+},[])
 
   function likePost(event){
+  updateTalkStatus(true)
   client.post(`/post/${postId}/like/`)
   .then(res => {
     updateId(_ => res.data.data.id)
     updateLikes(likes => [res.data.data, ...likes])
+    updateStyle({backgroundColor: blue});
   })
   .catch((err) => {
     console.log({error: err.message})
   })
+  .finally(() => updateTalkStatus(false))
   }
 
   function unlikePost(event){
+    updateTalkStatus(true)
+    console.log('test')
     client.delete(`/post/like/${id}`)
-    .then(res => {
+    .then(_ => {
       updateLikes(likes => likes.filter(like => like.id !== id))
+      updateStyle({backgroundColor: 'grey'})
     })
     .catch((err) => {
       console.log({error: err.message})
     })
+    .finally(() => updateTalkStatus(false))
   }
 
 function toggleLike(event) {
+  if (talkingToServer) return
   if (divStyle.backgroundColor === 'grey') likePost(event);
   else if (divStyle.backgroundColor !== 'grey') unlikePost(event);
-  const updatedStyle = divStyle.backgroundColor === `#1976d2` ? {backgroundColor: 'grey'} : {backgroundColor: '#1976d2'}
-  updateLike(updatedStyle);
 }
 
     return <div className = 'like-section'>
