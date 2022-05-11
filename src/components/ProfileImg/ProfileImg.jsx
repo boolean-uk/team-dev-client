a285456a8a8b43791cf67ff0ec93a3943dbe67da
Nico Picchio <nicolapicchio@gmail.com>
2022-05-11 15:45:37 +0100

    added context to error message and used local storage component

--- src/components/ProfileImg/ProfileImg.jsx
+++ src/components/ProfileImg/ProfileImg.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 import './style.css';
 import { useState, useEffect } from 'react';
 import client from '../../utils/client';
+import storage from '../../utils/storage';
 
 function ProfileImg({ avatar }) {
-  const userId = localStorage.getItem('userId');
   const [profileImg, setProfileImg] = useState('');
+  const localStorageResponse = storage.loadStorage()
+  const userId = localStorageResponse.userId;
 
   useEffect(() => {
     client
@@ -14,7 +16,9 @@ function ProfileImg({ avatar }) {
       .then((res) => {
         setProfileImg(res.data.data.user.profileImgUrl);
       })
-      .catch((err) => console.log(err.response));
+      .catch((err) => {
+        console.log('Something went wrong when loading the profile image...', err.response)
+      });
   }, []);
 
   if (avatar) {
