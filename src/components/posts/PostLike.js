4a3cc2fd9bec1ffe2784aec16234b8f65177b037
Paul Smart <pdsmart6@gmail.com>
2022-05-12 10:22:04 +0100

    fixed spacing

--- src/components/posts/PostLike.js
+++ src/components/posts/PostLike.js
@@ -23,32 +23,32 @@ useEffect(()=>{
   }
 },[]);
 
-  function likePost(event){
+function likePost(event){
+setTalkStatus(true);
+client.post(`/post/${postId}/like/`)
+.then(res => {
+  setId(_ => res.data.data.id);
+  setLikes(likes => [res.data.data, ...likes]);
+  setStyle({backgroundImage: blue});
+})
+.catch((err) => {
+  console.log({error: `when liking post: ${err.message}`});
+})
+.finally(() => setTalkStatus(false));
+}
+
+function unlikePost(event){
   setTalkStatus(true);
-  client.post(`/post/${postId}/like/`)
-  .then(res => {
-    setId(_ => res.data.data.id);
-    setLikes(likes => [res.data.data, ...likes]);
-    setStyle({backgroundImage: blue});
+  client.delete(`/post/like/${id}`)
+  .then(_ => {
+    setLikes(likes => likes.filter(like => like.id !== id));
+    setStyle({backgroundImage: grey});
   })
   .catch((err) => {
-    console.log({error: `when liking post: ${err.message}`});
+    console.log({error: `when unliking post: ${err.message}`});
   })
   .finally(() => setTalkStatus(false));
-  }
-
-  function unlikePost(event){
-    setTalkStatus(true);
-    client.delete(`/post/like/${id}`)
-    .then(_ => {
-      setLikes(likes => likes.filter(like => like.id !== id));
-      setStyle({backgroundImage: grey});
-    })
-    .catch((err) => {
-      console.log({error: `when unliking post: ${err.message}`});
-    })
-    .finally(() => setTalkStatus(false));
-  }
+}
 
 function toggleLike(event) {
   if (talkingToServer) return;
@@ -57,8 +57,8 @@ function toggleLike(event) {
 }
 
     return <div className = 'like-section'>
-    <p className = 'like-label'>Likes: {postLikes.length}</p>
-    <div className = 'icon-container' onClick={toggleLike} style={divStyle}>
+    <p className='like-label'>Likes: {postLikes.length}</p>
+    <div className='icon-container' onClick={toggleLike} style={divStyle}>
       <img className='like-img' src={like} alt='like'/>
     </div>
     </div>;
