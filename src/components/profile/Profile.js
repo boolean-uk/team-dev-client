51e87b172bc7e5018c0ee2ee1dd7d2f0ca892170
Paulo Barata <pbara7a@gmail.com>
2022-06-27 17:58:40 +0100

    Profile.js restored.

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -2,92 +2,199 @@ import { React, useEffect, useState } from 'react';
 import Button from '@mui/material/Button';
 import { Box } from '@mui/system';
 import { Stack } from '@mui/material';
-import { Link, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import TextField from '@mui/material/TextField';
 import Header from '../Header/Header';
 import client from '../../utils/client';
 
 const Profile = () => {
+  const [editingProfile, setEditingProfile] = useState(false);
+  const [editingPassword, setEditingPassword] = useState(false);
+  const [passwords, setPasswords] = useState({
+    currentPassword: '',
+    newPassword: '',
+    newPasswordConfirmation: '',
+  });
+
   const params = useParams();
   const [userData, setUserData] = useState({});
-  const [cohortsAvailable, setCohortsAvailable] = useState([])
 
   useEffect(() => {
     client
       .get(`/user/${params.id}`)
       .then((res) => setUserData(res.data.data.user))
-      .catch((err) => console.log(err.response));
+      .catch((err) => console.error(err.response));
   }, [params]);
 
-  useEffect(() => {
+  const handleChange = (event) => {
+    event.preventDefault();
+    const { value, name } = event.target;
+
+    if (editingProfile) {
+      setUserData({
+        ...userData,
+        [name]: value,
+      });
+    }
+
+    return false;
+  };
+
+  const handlePasswordChange = (event) => {
+    event.preventDefault();
+
+    const { value, name } = event.target;
+
+    setPasswords({
+      ...passwords,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
     client
-      .get('/cohort')
-      .then((res) => setCohortsAvailable(res.data.data))
-      .catch((err) => console.error(err.response));
-  }, [])
+      .patch(`/user/update/${userData.id}`, userData, false)
+      .then((res) => setUserData(res.data.data.user))
+      .catch((err) => console.error(err.response))
+      .finally(() => setEditingProfile(false));
+  };
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
+  const handlePasswordSubmit = async (event) => {
+    event.preventDefault();
+    const { newPassword, newPasswordConfirmation } = passwords;
 
-    client.patch(`/user/${userData.id}`, { cohort_id: e.target[0].value })
-  }
+    if (newPassword !== newPasswordConfirmation) {
+      return;
+    }
+  };
+
+  const fieldVariant = editingProfile ? 'outlined' : 'standard';
 
   return (
     <>
       <Header />
-      <div className='user-form'>
-        <TextField
-          className='user-form-input'
-          variant='outlined'
-          value={userData.first_name}
-        />
-        <TextField
-          className='user-form-input'
-          variant='outlined'
-          value={userData.last_name}
-        />
-        <TextField
-          className='user-form-input'
-          type='email'
-          variant='outlined'
-          value={userData.email}
-        />
+      {!editingPassword && (
+        <form className='user-form'>
+          <TextField
+            className='user-form-input'
+            label='First Name'
+            name='first_name'
+            value={userData.first_name}
+            onChange={handleChange}
+            variant={fieldVariant}
+          />
+          <TextField
+            className='user-form-input'
+            label='Last Name'
+            name='last_name'
+            value={userData.last_name}
+            onChange={handleChange}
+            variant={fieldVariant}
+          />
+          <TextField
+            className='user-form-input'
+            type='email'
+            label='Email'
+            name='email'
+            value={userData.email}
+            onChange={handleChange}
+            variant={fieldVariant}
+          />
 
-        <TextField
-          className='user-form-input'
-          variant='outlined'
-          value={userData.biography}
-        />
-        <TextField
-          className='user-form-input'
-          type='url'
-          variant='outlined'
-          value={userData.github_url}
-        />
+          <TextField
+            className='user-form-input'
+            label='Bio'
+            name='biography'
+            value={userData.biography}
+            onChange={handleChange}
+            variant={fieldVariant}
+          />
+          <TextField
+            className='user-form-input'
+            type='url'
+            label='GitHub URL'
+            name='github_url'
+            value={userData.github_url}
+            onChange={handleChange}
+            variant={fieldVariant}
+          />
+          {editingProfile && (
+            <Button
+              id='user-submit-button'
+              onClick={handleSubmit}
+              type='submit'
+              variant='contained'
+            >
+              Submit
+            </Button>
+          )}
 
-        <form onSubmit={handleSubmit}>
-          <span>Add student to cohort: </span> 
-          <select>
-          <option value={""} selected={userData.cohort_id === null ? "selected" : ""}>Please select a cohort</option>
-          {cohortsAvailable && cohortsAvailable.map((cohort) => (
-            <option key={cohort.id} value={cohort.id} selected={userData.cohort_id === cohort.id ? "selected" : ""}>{cohort.id}</option>
-          ))}
-        </select>
-        <button type='submit'>Confirm</button>
+          {!editingProfile && (
+            <Box>
+              <Stack spacing={2} direction='row'>
+                <Button
+                  variant='contained'
+                  onClick={() => setEditingProfile(true)}
+                >
+                  Edit Profile
+                </Button>{' '}
+              </Stack>
+            </Box>
+          )}
+
+          {!editingPassword && (
+            <Box>
+              <Stack spacing={2} direction='row'>
+                <Button
+                  variant='contained'
+                  onClick={() => setEditingPassword(true)}
+                >
+                  Edit Password
+                </Button>{' '}
+              </Stack>
+            </Box>
+          )}
         </form>
+      )}
 
-        <Button id='user-submit-button' type='submit' variant='contained'>
-          Submit
-        </Button>
-
-        <Box>
-          <Stack spacing={2} direction='row'>
-            <Link to='/edit-profile'>
-              <Button variant='contained'>Edit Profile</Button>{' '}
-            </Link>
-          </Stack>
-        </Box>
-      </div>
+      {editingPassword && (
+        <form>
+          <TextField
+            className='user-form-input'
+            type='text'
+            label='current password'
+            name='currentPassword'
+            value={userData.github_url}
+            onChange={handlePasswordChange}
+          />
+          <TextField
+            className='user-form-input'
+            type='text'
+            label='new password'
+            name='newPassword'
+            value={userData.github_url}
+            onChange={handlePasswordChange}
+          />
+          <TextField
+            className='user-form-input'
+            type='text'
+            label='new password'
+            name='newPasswordConfirmation'
+            value={userData.github_url}
+            onChange={handlePasswordChange}
+          />
+          <Button
+            id='user-submit-button'
+            onClick={handlePasswordSubmit}
+            type='submit'
+            variant='contained'
+          >
+            Submit
+          </Button>
+        </form>
+      )}
     </>
   );
 };
