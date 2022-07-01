1185ca7cd36b7f0f6124749361ec62877d67b251
Tibor22 <tothtibitravel@gmail.com>
2022-07-01 15:36:45 +0100

    save gitignore

--- src/Helper/dateFormatter.js
+++ src/Helper/dateFormatter.js
@@ -1,16 +1,32 @@
+// export const formatDate = (date) => {
+// 	date = new Date(date);
+// 	const newDate =
+// 		date.getDate() +
+// 		'/' +
+// 		(date.getMonth() + 1) +
+// 		'/' +
+// 		date.getFullYear() +
+// 		' ' +
+// 		(date.getHours() > 12 ? date.getHours() - 12 : date.getHours()) +
+// 		':' +
+// 		date.getMinutes() +
+// 		' ' +
+// 		(date.getHours() >= 12 ? 'PM' : 'AM');
+// 	return newDate;
+// };
+
 export const formatDate = (date) => {
-	date = new Date(date);
+	const d = new Date(date);
 	const newDate =
-		date.getDate() +
-		'/' +
-		(date.getMonth() + 1) +
-		'/' +
-		date.getFullYear() +
+		('0' + d.getDate()).slice(-2) +
+		'-' +
+		('0' + (d.getMonth() + 1)).slice(-2) +
+		'-' +
+		d.getFullYear() +
 		' ' +
-		(date.getHours() > 12 ? date.getHours() - 12 : date.getHours()) +
+		('0' + d.getHours()).slice(-2) +
 		':' +
-		date.getMinutes() +
-		' ' +
-		(date.getHours() >= 12 ? 'PM' : 'AM');
+		('0' + d.getMinutes()).slice(-2);
+
 	return newDate;
 };
