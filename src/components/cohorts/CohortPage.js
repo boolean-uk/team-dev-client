25e7cebb632c700db58fa4e290e12b50f54368d1
Dan <dannorthcott8@gmail.com>
2022-07-06 16:18:54 +0100

    altered client side to only make one api call to server

--- src/components/cohorts/CohortPage.js
+++ src/components/cohorts/CohortPage.js
@@ -13,22 +13,12 @@ const CohortPage = () => {
 
   useEffect(() => {
     client
-        .get(`/cohort/${params.id}`)
-        .then((res) => setCohortName(res.data.data.cohortName))
-        .catch((err) => console.error(err.response));
-  }, [params]);
-
-  useEffect(() => {
-    client
-        .get(`/user?cohort_id=${params.id}`)
-        .then((res) => setStudentsInCohort(res.data.data.users))
-        .catch((err) => console.error(err.response));
-  }, [params]);
-
-  useEffect(() => {
-    client
-        .get(`/user?cohort=false`)
-        .then((res) => setAvailibleUsers(res.data.data.users))
+        .get(`/cohort/${params.id}?availableStudents=true`)
+        .then((res) => {
+          setCohortName(res.data.data.cohortName)
+          setStudentsInCohort(res.data.data.users)
+          setAvailibleUsers(res.data.data.availableStudents)
+        })
         .catch((err) => console.error(err.response));
   }, [params]);
 
@@ -37,11 +27,11 @@ const CohortPage = () => {
 
     client.patch(`/user/${userId}`, { cohort_id: +params.id })
     .then((res) => {
-      const targetStudent = availibleUsers.find((user) => user.id === userId)
+      const targetStudent = availibleUsers.find((user) => user.user.id === userId)
       const updatedStudentsInCohort = [...studentsInCohort, targetStudent]
       setStudentsInCohort(updatedStudentsInCohort)
 
-      const updatedAvailibleUsers = availibleUsers.filter((user) => user.id !== userId)
+      const updatedAvailibleUsers = availibleUsers.filter((user) => user.user.id !== userId)
       setAvailibleUsers(updatedAvailibleUsers)
     })
     .catch((err) => console.error(err.response));
@@ -61,16 +51,16 @@ const CohortPage = () => {
             <h2>Cohort Name: {cohortName}</h2>
             {studentsInCohort?.map((user, index) => (
               <div className='student' key={index}>
-                <h3>{user.first_name} {user.last_name}</h3> 
+                <h3>{user.user.first_name} {user.user.last_name}</h3> 
                   <img 
                     className='profile-picture' 
-                    src={user.profile_url} 
+                    src={user.user.profile_url} 
                     alt='user profile pic' 
-                    onClick={(e) => handleClick(e, user.id)}
+                    onClick={(e) => handleClick(e, user.user.id)}
                   />
                 <ul>
-                  <li><i>Email: {user.email}</i> </li>
-                  <li><i>GitHub: {user.github_url}</i></li>
+                  <li><i>Email: {user.user.email}</i> </li>
+                  <li><i>GitHub: {user.user.github_url}</i></li>
                 </ul>
               </div>
             ))}
@@ -80,8 +70,8 @@ const CohortPage = () => {
             <h2>Availible Users</h2>
             {availibleUsers?.map((user, index) => (
               <div key={index}>
-                <p>{user.first_name} {user.last_name}</p>
-               	<button onClick={(e) => handleSubmitAddStudentToCohort(e, user.id)}>Add to Cohort</button>
+                <p>{user.user.first_name} {user.user.last_name}</p>
+               	<button onClick={(e) => handleSubmitAddStudentToCohort(e, user.user.id)}>Add to Cohort</button>
               </div>
             ))}
           </div>
