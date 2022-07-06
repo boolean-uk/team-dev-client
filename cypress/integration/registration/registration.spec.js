c8124e860c2f72516bcc4432ab08f3b68b0f5d89
UnknownCreator1 <90321114+UnknownCreator1@users.noreply.github.com>
2022-07-06 16:18:19 +0100

    User is automatically logged in after register New #4

--- cypress/integration/registration/registration.spec.js
+++ cypress/integration/registration/registration.spec.js
@@ -17,19 +17,5 @@ describe('User Registration', () => {
       cy.get('input[name=github_url]').type('https://github.com/dearshrewdwit/')
       cy.get('#user-submit-button').click()
     })
-
-    it('user is registered and automatically logged in and be redirected to the home page', () => {
-      cy.get('#user-registration-link').click()
-      cy.url().should('eq', `${Cypress.config('baseUrl')}/signup`)
-
-      cy.get('input[name=first_name]').type('Edward')
-      cy.get('input[name=last_name]').type('Withers')
-      cy.get('input[name=email]').type('test@test.com')
-      cy.get('input[name=password]').type('test12')
-      cy.get('input[name=biography]').type('a long bio')
-      cy.get('input[name=github_url]').type('https://github.com/dearshrewdwit')
-      cy.get('#user-submit-button').click()
-
-      cy.url().should('eq', `${Cypress.config('baseUrl')}/home`)
-    })
+})
 })
