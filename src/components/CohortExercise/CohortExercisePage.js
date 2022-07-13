8a0657600f3c19db1797b0a9663cb72017abcb1d
sigristarisa <arisasigrist.ch@gmail.com>
2022-07-13 13:33:33 +0200

    able to get cohortExercises

---
+++ src/components/CohortExercise/CohortExercisePage.js
@@ -0,0 +1,29 @@
+import Header from '../Header/Header'
+import { useState, useEffect, useContext } from 'react'
+import { loggedInUserContext } from "../../Helper/loggedInUserContext";
+import client from '../../utils/client'
+
+export default function ExercisePage() {
+  const { loggedInUser } = useContext(loggedInUserContext);
+  const [cohortExercises, setCohortExercises] = useState([])
+
+
+    useEffect(() => {
+      client
+        .get(`/cohort/${loggedInUser.cohort_id}/cohortExercises`)
+        .then((res) => setCohortExercises(res.data.data.cohortExercises))
+        .catch((err) => console.error(err.response));
+    }, []);
+
+    console.log("cohort Exercises are: ", cohortExercises)
+
+
+
+    return (
+    <>
+    <Header companyName={`Cohort Manager 2.0`}/>
+   <ul>
+
+   </ul>
+    </>    )
+}
