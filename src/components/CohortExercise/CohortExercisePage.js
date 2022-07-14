88822b94995061710424234efb97552396b8e33c
sigristarisa <arisasigrist.ch@gmail.com>
2022-07-14 13:17:13 +0200

    created sortToUnits and createUnitsArray function

--- src/components/CohortExercise/CohortExercisePage.js
+++ src/components/CohortExercise/CohortExercisePage.js
@@ -7,34 +7,74 @@ import './CohortExercisePage.css'
 export default function ExercisePage() {
   const { loggedInUser } = useContext(loggedInUserContext);
   const [cohortExercises, setCohortExercises] = useState([]);
+  const [units, setUnits] = useState([])
 
-  const unitOne = [
-    'Exercise one',
-    'Exercise two',
-    'Exercise three',
-    'Exercise four',
-    'Exercise five',
-    'Exercise six',
-    'Exercise seven',
-    'Exercise eight'
-  ];
+  const sortToUnits = () => {
+    cohortExercises.sort((cohortExercise, nextCohortExercise) => {
+        const unitId = cohortExercise.cohortExercise.exercise.unitId
+        const nextUnitId = nextCohortExercise.cohortExercise.exercise.unitId
+        return unitId - nextUnitId
+    })
+}
 
-  const unitTwo = ['Exercise one', 'Exercise two'];
+  const createUnitsArray = () => {
+    sortToUnits()
+    const unitsArray = []
+    for(let i = 0; i < cohortExercises.length-1; i++) {
+        let unitArray = []
+        const currentUnitId = cohortExercises[i].cohortExercise.exercise.unitId
+        const nextUnitId = cohortExercises[i+1].cohortExercise.exercise.unitId
+       
+        if (currentUnitId === nextUnitId) {
+            unitArray.push(cohortExercises[i])
+        } else {
+            unitsArray.push(unitArray)
+            console.log("in the else statement: ", unitsArray)
+            unitArray = []
+        }
+        console.log("out of for loop: ", unitArray)
+    }  
+    console.log("let's check the unitsArray: ", unitsArray)  
+    setUnits(unitsArray)
+  } 
 
-  const unitThree = [
-    'Exercise one',
-    'Exercise two',
-    'Exercise three',
-    'Exercise four',
-    'Exercise five',
-  ];
+// loop through cohortExercises
+// create currentUnitId and nextUnitId
+// check if the next cohortExercise is the same as currentId
+// if it is the same, push it to an empty array
+// if not, reset the array empty and create a new array, push the cohortExercise
+// at the end, push all the created array into unitsArray
 
-  const unitArr = [unitOne, unitTwo, unitThree];
+
+//   create a function that pushes the created arrays into unit array
+
+
+//   const unitOne = [
+//     'Exercise one',
+//     'Exercise two',
+//     'Exercise three',
+//     'Exercise four',
+//     'Exercise five',
+//     'Exercise six',
+//     'Exercise seven',
+//     'Exercise eight'
+//   ];
+
+//   const unitTwo = ['Exercise one', 'Exercise two'];
+
+//   const unitThree = [
+//     'Exercise one',
+//     'Exercise two',
+//     'Exercise three',
+//     'Exercise four',
+//     'Exercise five',
+//   ];
 
   useEffect(() => {
     client
       .get(`/cohort/${loggedInUser.cohort_id}/cohortExercises`)
-      .then((res) => setCohortExercises(res.data.data.cohortExercises))
+      .then((res) =>setCohortExercises(res.data.data.cohortExercises))
+      .then(() => createUnitsArray())
       .catch((err) => console.error(err.response));
   }, []);
 
@@ -42,16 +82,18 @@ export default function ExercisePage() {
     <div>
       <Header companyName={`Cohort Manager 2.0`} />
       <div className='cohortExercise-container'>
-        {unitArr.map((unit, unitIndex) => (
-          <div className='unit-container' key={unitIndex}>
-            {/* Later replace unit name */}
-            <h2>Unit Name</h2> 
-            <ul className='grid-three-columns'>
+        {units.map((unit, unitIndex) => (
+          <ul className='unit-container' key={unitIndex}>
+            <li>
+             {/* Later replace unit name */}
+             <h2>Unit Name</h2> 
+             <ul className='grid-three-columns'>
               {unit.map((exercise, exerciseIndex) => (
                 <li className='exercise-container' key={exerciseIndex}>{exercise}</li>
               ))}
-            </ul>
-          </div>
+             </ul>
+            </li>
+          </ul>
         ))}
       </div>
     </div>
