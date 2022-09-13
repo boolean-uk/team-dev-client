bfc7e8f08fd17a30602179c17ead5bb2c55b527a
deliailu27 <54033364+deliailu27@users.noreply.github.com>
2022-09-13 18:32:25 +0100

    conditional rendered button finished

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,14 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { Stack } from '@mui/material';
 import InputBase from '@mui/material/InputBase';
+import { useState } from 'react';
 
-const Header = ({ companyName }) => {
+const Header = ({ companyName, isTeacher}) => {
+  
+
+ console.log("header teacher check:",isTeacher)
+
+  
   return (
     <>
       <Box
@@ -35,7 +41,7 @@ const Header = ({ companyName }) => {
 
         <Box>
           <Stack spacing={2} direction='row'>
-          <Button variant='contained'>Add Cohort</Button>
+          {isTeacher && <Button variant='contained'>Create Cohort</Button>}
           <Button variant='contained'>Logout</Button>
           </Stack>
         </Box>
--- src/components/posts/PostsPage.js
+++ src/components/posts/PostsPage.js
@@ -1,25 +1,44 @@
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import PostForm from './PostForm';
-import client from '../../utils/client';
-import './style.css';
+import { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import PostForm from "./PostForm";
+import client from "../../utils/client";
+import "./style.css";
+import jwt_decode from "jwt-decode";
 
-import Header from '../Header/Header';
+import Header from "../Header/Header";
 
 const PostsPage = () => {
-  const [post, setPost] = useState({ content: '' });
-  const [postResponse, setPostResponse] = useState('');
+  const [post, setPost] = useState({ content: "" });
+  const [postResponse, setPostResponse] = useState("");
   const [posts, setPosts] = useState([]);
+  const [isTeacher, setIsTeacher] = useState(false);
+  const tokenKey = process.env.REACT_APP_USER_TOKEN;
+
   let navigate = useNavigate();
 
   useEffect(() => {
-    client.get('/posts').then((res) => setPosts(res.data.data.posts));
+    const token = localStorage.getItem(tokenKey);
+    const decoded = jwt_decode(token);
+    console.log(decoded);
+    let id = decoded.userId;
+
+    client.get(`/user/${id}`).then((res) => {
+      console.log("res", res);
+      if (res.data.data.user.role === "TEACHER") {
+        setIsTeacher(true);
+      }
+      console.log(isTeacher);
+    });
+
+    client.get("/posts").then((res) => {
+      setPosts(res.data.data.posts);
+    });
   }, []);
 
   const createPost = async (event) => {
     event.preventDefault();
     client
-      .post('/post', post)
+      .post("/post", post)
       .then((res) => setPostResponse(res.data))
       .catch((data) => {
         console.log(data);
@@ -37,22 +56,23 @@ const PostsPage = () => {
 
   const signOut = (event) => {
     event.preventDefault();
-    localStorage.setItem(process.env.REACT_APP_USER_TOKEN, '');
-    navigate('../', { replace: true });
+    localStorage.setItem(process.env.REACT_APP_USER_TOKEN, "");
+    navigate("../", { replace: true });
   };
 
   return (
     <>
-      <Header companyName={`Cohort Manager 2.0`} />
-      <section className='posts-section'>
-        <button id='user-signout-button' onClick={signOut}>
+      <Header companyName={`Cohort Manager 2.0`} isTeacher={isTeacher} />
+      <section className="posts-section">
+        <button id="user-signout-button" onClick={signOut}>
           sign out
         </button>
+
         <p>Status: {postResponse.status}</p>
         <PostForm handleSubmit={createPost} handleChange={handleChange} />
-        <ul className='posts-list'>
+        <ul className="posts-list">
           {posts.map((post, index) => (
-            <li key={index} className='post-item'>
+            <li key={index} className="post-item">
               {post.content}
             </li>
           ))}
