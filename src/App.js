7d1d04030c16560d4a02a0a12c29104b1aee1990
Hubert Lemczak <hubertlemczak@gmail.com>
2022-09-13 17:08:00 +0100

    refactor: create global header after authorization

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import RegistrationPage from './components/users/registration/RegistrationPage';
 import PostsPage from './components/posts/PostsPage';
 
 import { Routes, Route, Navigate, Outlet } from 'react-router-dom';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Routes>
-        <Route path='/' element={<LoginPage />} />
-        <Route path='/signup' element={<RegistrationPage />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<RegistrationPage />} />
         <Route element={<AuthenticateUser />}>
-          <Route path='/posts' element={<PostsPage />} />
+          <Route path="/posts" element={<PostsPage />} />
         </Route>
       </Routes>
     </div>
@@ -31,5 +32,5 @@ const AuthenticateUser = ({ children, redirectPath = '/' }) => {
     return <Navigate to={redirectPath} replace />;
   }
 
-  return <Outlet />;
+  return <Header companyName={`Cohort Manager 2.0`} />;
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { Stack } from '@mui/material';
 import InputBase from '@mui/material/InputBase';
+import { Outlet } from 'react-router-dom';
 
 const Header = ({ companyName }) => {
   return (
@@ -14,32 +15,41 @@ const Header = ({ companyName }) => {
           justifyContent: 'space-between',
           alignContent: 'center',
           width: '100vw',
-          padding: '1em'
+          padding: '1em',
         }}
       >
-
         <Box>
           <Typography>
             <p>{companyName}</p>
           </Typography>
         </Box>
 
-        <Box sx={{ display: 'flex', justifyContent: 'center', alignContent: 'center' }}>
-          <Box sx={{backgroundColor: 'white'}}>
-          <InputBase placeholder='Search…' inputProps={{ 'aria-label': 'search' }} />
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignContent: 'center',
+          }}
+        >
+          <Box sx={{ backgroundColor: 'white' }}>
+            <InputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
           </Box>
           <Box>
-            <Button variant='contained'>Search User</Button>
+            <Button variant="contained">Search User</Button>
           </Box>
         </Box>
 
         <Box>
-          <Stack spacing={2} direction='row'>
-          <Button variant='contained'>Add Cohort</Button>
-          <Button variant='contained'>Logout</Button>
+          <Stack spacing={2} direction="row">
+            <Button variant="contained">Add Cohort</Button>
+            <Button variant="contained">Logout</Button>
           </Stack>
         </Box>
       </Box>
+      <Outlet />
     </>
   );
 };
