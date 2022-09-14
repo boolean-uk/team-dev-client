78476808a0a070e96b98e25ea7cd45d834282c6f
Ning Ma <ningma905@gmail.com>
2022-09-14 10:09:01 +0100

    feat: get all formatted posts when the components mounts for the first time

--- src/components/posts/PostsPage.js
+++ src/components/posts/PostsPage.js
@@ -1,25 +1,31 @@
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import PostForm from './PostForm';
-import client from '../../utils/client';
-import './style.css';
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import PostForm from "./PostForm";
+import client from "../../utils/client";
+import "./style.css";
 
-import Header from '../Header/Header';
+import Header from "../Header/Header";
+import { getAllPosts, getAllPostsFormatted } from "./utils/getAllPosts";
 
 const PostsPage = () => {
-  const [post, setPost] = useState({ content: '' });
-  const [postResponse, setPostResponse] = useState('');
+  const [post, setPost] = useState({ content: "" });
+  const [postResponse, setPostResponse] = useState("");
   const [posts, setPosts] = useState([]);
   let navigate = useNavigate();
 
   useEffect(() => {
-    client.get('/posts').then((res) => setPosts(res.data.data.posts));
+    const getAllPosts = async () => {
+      const formattedPosts = await getAllPostsFormatted();
+      setPosts(formattedPosts);
+    };
+
+    getAllPosts();
   }, []);
 
   const createPost = async (event) => {
     event.preventDefault();
     client
-      .post('/post', post)
+      .post("/post", post)
       .then((res) => setPostResponse(res.data))
       .catch((data) => {
         console.log(data);
@@ -37,22 +43,22 @@ const PostsPage = () => {
 
   const signOut = (event) => {
     event.preventDefault();
-    localStorage.setItem(process.env.REACT_APP_USER_TOKEN, '');
-    navigate('../', { replace: true });
+    localStorage.setItem(process.env.REACT_APP_USER_TOKEN, "");
+    navigate("../", { replace: true });
   };
 
   return (
     <>
       <Header companyName={`Cohort Manager 2.0`} />
-      <section className='posts-section'>
-        <button id='user-signout-button' onClick={signOut}>
+      <section className="posts-section">
+        <button id="user-signout-button" onClick={signOut}>
           sign out
         </button>
         <p>Status: {postResponse.status}</p>
         <PostForm handleSubmit={createPost} handleChange={handleChange} />
-        <ul className='posts-list'>
+        <ul className="posts-list">
           {posts.map((post, index) => (
-            <li key={index} className='post-item'>
+            <li key={index} className="post-item">
               {post.content}
             </li>
           ))}
