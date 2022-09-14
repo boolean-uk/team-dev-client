76d8e5b77fe631dab2e898650fe4457cd1e94e17
John Xander <johnblox5@gmail.com>
2022-09-14 11:49:12 +0100

    feat: fetch user data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import LoginPage from './components/users/login/LoginPage';
 import RegistrationPage from './components/users/registration/RegistrationPage';
@@ -8,19 +8,31 @@ import Profile from './components/profile/Profile';
 import { Routes, Route, Navigate, Outlet } from 'react-router-dom';
 
 function App() {
-  const [profileData] = useState({
+
+  const [user, setUser] = useState({
     first_name: "Nathan",
     last_name: "King",
     biography: "Hello world",
     github_url: "https://github.com/vherus"
   })
 
+  const [token] = useState(`Bearer ${process.env.REACT_APP_USER_TOKEN}`)
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}/user/4`, {
+      method: "GET",
+      headers: { Authorization: token }
+    })
+      .then(resp => resp.json())
+      .then(data => setUser(data.data.user))
+  }, [])
+
   return (
     <div className='App'>
       <Routes>
         <Route path='/' element={<LoginPage />} />
         <Route path='/signup' element={<RegistrationPage />} />
-        <Route path='/profile' element={<Profile profileData={profileData} />} />
+        <Route path='/profile' element={<Profile profileData={user} />} />
         <Route element={<AuthenticateUser />}>
           <Route path='/posts' element={<PostsPage />} />
         </Route>
