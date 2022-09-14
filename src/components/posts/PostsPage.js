e4200c7136a2116e5af73ea1934dc58e07804a1a
Ning Ma <ningma905@gmail.com>
2022-09-14 15:57:20 +0100

    chore: remove brackets

--- src/components/posts/PostsPage.js
+++ src/components/posts/PostsPage.js
@@ -53,15 +53,13 @@ const PostsPage = () => {
         </button>
         <p>Status: {postResponse.status}</p>
         <PostForm handleSubmit={createPost} handleChange={handleChange} />
-        {posts.length > 0 ? (
+        {posts.length > 0 ?
           <ul className='posts-list'>
-            {posts.map((post, index) => (
-              <PostItem post={post} key={index} />
-            ))}
+            {posts.map((post, index) => <PostItem post={post} key={index} />)}
           </ul>
-        ) : (
+          :
           <p className='no-posts-message'>There are no posts at the moment.</p>
-        )}
+        }
       </section>
     </>
   );
