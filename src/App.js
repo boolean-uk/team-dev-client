147f2755e7caee480897b0626830c1f96620ab4b
Lorel <lorel.oboue@gmail.com>
2022-09-15 14:58:44 +0100

    working on registration profile pic url

--- src/App.js
+++ src/App.js
@@ -1,55 +1,56 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import jwt_decode from "jwt-decode";
-import './App.css';
-import LoginPage from './components/users/login/LoginPage';
-import RegistrationPage from './components/users/registration/RegistrationPage';
-import PostsPage from './components/posts/PostsPage';
-import Profile from './components/profile/Profile';
+import "./App.css";
+import LoginPage from "./components/users/login/LoginPage";
+import RegistrationPage from "./components/users/registration/RegistrationPage";
+import PostsPage from "./components/posts/PostsPage";
+import Profile from "./components/profile/Profile";
 
-import { Routes, Route, Navigate, Outlet } from 'react-router-dom';
+import { Routes, Route, Navigate, Outlet } from "react-router-dom";
 
 function App() {
-
   const [user, setUser] = useState({
     first_name: "Nathan",
     last_name: "King",
     biography: "Hello world",
-    github_url: "https://github.com/vherus"
-  })
+    github_url: "https://github.com/vherus",
+  });
 
-  const [token] = useState(`Bearer ${localStorage.getItem(process.env.REACT_APP_USER_TOKEN)}`)
+  const [token] = useState(
+    `Bearer ${localStorage.getItem(process.env.REACT_APP_USER_TOKEN)}`
+  );
 
   useEffect(() => {
-    const userId = getLoggedInUserId()
+    const userId = getLoggedInUserId();
     if (userId === null) {
-      return
+      return;
     }
     fetch(`${process.env.REACT_APP_API_URL}/user/${userId}`, {
       method: "GET",
-      headers: { Authorization: token }
+      headers: { Authorization: token },
     })
-      .then(resp => resp.json())
-      .then(data => setUser(data.data.user))
+      .then((resp) => resp.json())
+      .then((data) => setUser(data.data.user));
     // eslint-disable-next-line
-  }, [])
+  }, []);
 
   const getLoggedInUserId = () => {
-    const loadedToken = localStorage.getItem('token');
-    if (loadedToken === null) {
-      return null
+    const loadedToken = localStorage.getItem("token");
+    if (loadedToken === null || loadedToken === "") {
+      return null;
     }
-    const decoded = jwt_decode(loadedToken)
-    return decoded.userId
-  }
+    const decoded = jwt_decode(loadedToken);
+    return decoded.userId;
+  };
 
   return (
-    <div className='App'>
+    <div className="App">
       <Routes>
-        <Route path='/' element={<LoginPage />} />
-        <Route path='/signup' element={<RegistrationPage />} />
-        <Route path='/profile' element={<Profile profileData={user} />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<RegistrationPage />} />
+        <Route path="/profile" element={<Profile profileData={user} />} />
         <Route element={<AuthenticateUser />}>
-          <Route path='/posts' element={<PostsPage />} />
+          <Route path="/posts" element={<PostsPage />} />
         </Route>
       </Routes>
     </div>
@@ -57,13 +58,13 @@ function App() {
 }
 
 function isLoggedIn() {
-  const loadedToken = localStorage.getItem('token');
-  return !(loadedToken === '');
+  const loadedToken = localStorage.getItem("token");
+  return !(loadedToken === "");
 }
 
 export default App;
 
-const AuthenticateUser = ({ children, redirectPath = '/' }) => {
+const AuthenticateUser = ({ children, redirectPath = "/" }) => {
   if (!isLoggedIn()) {
     return <Navigate to={redirectPath} replace />;
   }
