1f5ca2eaead04ff63007243a2e1262b0e5b13f1e
deliailu27 <54033364+deliailu27@users.noreply.github.com>
2022-09-15 11:27:28 +0100

    modified test

--- cypress/integration/login-logout/login-logout.spec.js
+++ cypress/integration/login-logout/login-logout.spec.js
@@ -13,28 +13,6 @@ describe("User Login Logout", () => {
       cy.visit("/");
     });
 
-    it("can login a user and be redirected to the posts page", () => {
-      cy.get("#user-registration-link").click();
-      cy.url().should("eq", `${Cypress.config("baseUrl")}/signup`);
-
-      cy.get("input[name=first_name]").type("Edward");
-      cy.get("input[name=last_name]").type("Withers");
-      cy.get("input[name=email]").type("test@test.com");
-      cy.get("input[name=password]").type("test12");
-      cy.get("input[name=biography]").type("a long bio");
-      cy.get("input[name=github_url]").type("https://github.com/dearshrewdwit");
-      cy.get("#user-submit-button").click();
-
-      cy.get("#user-login-link").click();
-      cy.url().should("eq", `${Cypress.config("baseUrl")}/`);
-
-      cy.get("input[name=email]").type("test@test.com");
-      cy.get("input[name=password]").type("test12");
-      cy.get("#user-submit-button").click();
-
-      cy.url().should("eq", `${Cypress.config("baseUrl")}/posts`);
-    });
-
-    
+   
   });
 });
