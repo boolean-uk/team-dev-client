59cec491fd724b9db0447289ddfbc716f2a7363d
Hubert Lemczak <hubertlemczak@gmail.com>
2022-09-15 13:03:38 +0100

    fix: create assets dir for default profile picture

--- src/components/posts/PostItem.js
+++ src/components/posts/PostItem.js
@@ -1,22 +1,24 @@
+import defaultPFP from "../../assets/default.png";
+
 const PostItem = ({ post }) => {
   return (
-    <li className='post-item'>
-      <div className='post-header-wrap'>
-        <div className='post-profile-wrap'>
+    <li className="post-item">
+      <div className="post-header-wrap">
+        <div className="post-profile-wrap">
           <img
-            className='post-profile-img'
-            src={post.user.profile.profileImageUrl}
-            alt='profile'
+            className="post-profile-img"
+            src={post.user.profile.profileImageUrl || defaultPFP}
+            alt="profile"
           />
           <h3>
             {post.user.profile.firstName} {post.user.profile.lastName}
           </h3>
         </div>
 
-        <p className='createdAt-time'>{post.createdAt}</p>
+        <p className="createdAt-time">{post.createdAt}</p>
       </div>
 
-      <p className='post-content'>{post.content}</p>
+      <p className="post-content">{post.content}</p>
 
       <div className="modify-btn-wrap">
         <button className="modify-btn">Edit</button>
