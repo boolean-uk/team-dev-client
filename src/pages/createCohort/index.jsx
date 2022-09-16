7b985756a1aecad1f6887eb2438ea9ca1f489348
deliailu27 <54033364+deliailu27@users.noreply.github.com>
2022-09-16 12:20:30 +0100

    formatting

--- src/pages/createCohort/index.jsx
+++ src/pages/createCohort/index.jsx
@@ -1,6 +1,3 @@
-import { Box, Button } from "@mui/material"
-import { useState } from "react";
-import client from "../../utils/client";
 import CohortList from "./components/cohortList"
 import CreateCohortByName from "./components/createCohortByName";
 import "./style.css"
