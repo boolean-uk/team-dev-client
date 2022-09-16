68ba257e381aa2df29d48c73835aa38c4dbee70a
John Xander <johnblox5@gmail.com>
2022-09-16 11:42:57 +0100

    fix: console.log removed

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -35,8 +35,6 @@ const Profile = ({ getLoggedInUserId, user, setUser }) => {
         event.preventDefault()
         const { value, name } = event.target
 
-        console.log(event.target)
-
         setUser({
             ...user,
             [name]: value,
