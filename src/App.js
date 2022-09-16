426ea9520dbdb57e79bee182de50783647f1a8f3
Thomas Brockington <xtombrock1989@gmail.com>
2022-09-16 15:35:41 +0100

    single quotes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import { useEffect, useState } from "react";
-import jwt_decode from "jwt-decode";
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import jwt_decode from 'jwt-decode';
 
-import "./App.css";
+import './App.css';
 
-
-import LoginPage from "./components/users/login/LoginPage";
-import RegistrationPage from "./components/users/registration/RegistrationPage";
-import PostsPage from "./components/posts/PostsPage";
-import Profile from "./components/profile/Profile";
-import EnrolmentPage from "./pages/enrollment";
-import Header from "./components/Header/Header";
-import client from "./utils/client";
+import LoginPage from './components/users/login/LoginPage';
+import RegistrationPage from './components/users/registration/RegistrationPage';
+import PostsPage from './components/posts/PostsPage';
+import Profile from './components/profile/Profile';
+import EnrolmentPage from './pages/enrollment';
+import Header from './components/Header/Header';
+import client from './utils/client';
 
 function App() {
   const [user, setUser] = useState({
-
-    first_name: "Nathan",
-    last_name: "King",
-    biography: "Hello world",
-    github_url: "https://github.com/vherus",
+    first_name: 'Nathan',
+    last_name: 'King',
+    biography: 'Hello world',
+    github_url: 'https://github.com/vherus',
   });
-  const [foundUser, setFoundUser] = useState({})
+  const [foundUser, setFoundUser] = useState({});
 
   useEffect(() => {
     const userId = getLoggedInUserId();
@@ -36,18 +34,18 @@ function App() {
   }, []);
 
   useEffect(() => {
-    const foundUserId = getFoundUserId()
+    const foundUserId = getFoundUserId();
     if (foundUserId === null) {
       return;
     }
-    client 
+    client
       .get(`/user/${foundUserId}`)
       .then(res => setFoundUser(res.data.data.user))
       .catch(err => console.log(err));
-  }, [])
+  }, []);
 
   const getFoundUserId = () => {
-    const searchId = localStorage.getItem("id");
+    const searchId = localStorage.getItem('id');
     console.log('searchId', searchId);
     if (searchId === null) {
       return null;
@@ -56,7 +54,7 @@ function App() {
   };
 
   const getLoggedInUserId = () => {
-    const loadedToken = localStorage.getItem("token");
+    const loadedToken = localStorage.getItem('token');
     if (loadedToken === null) {
       return null;
     }
@@ -67,17 +65,27 @@ function App() {
   return (
     <div className="App">
       <Routes>
-
         <Route path="/" element={<LoginPage />} />
         <Route path="/signup" element={<RegistrationPage />} />
 
         <Route element={<AuthenticateUser />}>
           <Route path="/posts" element={<PostsPage />} />
           <Route path="/enrolment" element={<EnrolmentPage />} />
-          <Route path="/user/:id/profile" element={<Profile profileData={foundUser} />} />
-          <Route path="/profile" element={
-            <Profile profileData={user} getLoggedInUserId={getLoggedInUserId} user={user} setUser={setUser} />
-          } />
+          <Route
+            path="/user/:id/profile"
+            element={<Profile profileData={foundUser} />}
+          />
+          <Route
+            path="/profile"
+            element={
+              <Profile
+                profileData={user}
+                getLoggedInUserId={getLoggedInUserId}
+                user={user}
+                setUser={setUser}
+              />
+            }
+          />
         </Route>
       </Routes>
     </div>
@@ -85,13 +93,13 @@ function App() {
 }
 
 function isLoggedIn() {
-  const loadedToken = localStorage.getItem("token");
-  return !(loadedToken === "");
+  const loadedToken = localStorage.getItem('token');
+  return !(loadedToken === '');
 }
 
 export default App;
 
-const AuthenticateUser = ({ children, redirectPath = "/" }) => {
+const AuthenticateUser = ({ children, redirectPath = '/' }) => {
   if (!isLoggedIn()) {
     return <Navigate to={redirectPath} replace />;
   }
