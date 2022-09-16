b64a7a43695c18a56fb193ae6adb068bda23a26a
deliailu27 <54033364+deliailu27@users.noreply.github.com>
2022-09-16 11:55:35 +0100

    added textfiled

--- src/pages/createCohort/components/cohortList.jsx
+++ src/pages/createCohort/components/cohortList.jsx
@@ -1,11 +1,33 @@
+import { Card } from "@mui/material";
+import { useEffect } from "react";
+import { useState } from "react";
 import client from "../../../utils/client";
+import "./style.css"
 
-const cohortList=()=>{
-    client.get('/cohort').then((res)=>{console.log(res)})
+const CohortList=()=>{
+    const [cohorts,setCohorts]=useState([])
+
+    useEffect(()=>{
+        client.get('/cohort').then((res)=>{setCohorts(res.data.data.cohorts)})
+    
+        console.log('cohorts',cohorts)
+    },[]);
+   
 
 
-    return (<><p>test</p></>)
+    return (
+    <>
+    <div className="cohort-list">
+     {cohorts.map(cohort=>{return (
+     <Card className="cohort-card" key={`${cohort.id}`}>
+        {`cohort ${cohort.id} - cohort name`}
+    
+     </Card>
+      )}
+    )}
+    </div>
+    </>)
 }
 
 
-export default cohortList;
+export default CohortList;
