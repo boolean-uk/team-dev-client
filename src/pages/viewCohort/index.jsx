bb31875277236cfef2d2cb634657000ef9685159
Hubert Lemczak <hubertlemczak@gmail.com>
2022-09-20 13:00:26 +0100

    feat: loading state

--- src/pages/viewCohort/index.jsx
+++ src/pages/viewCohort/index.jsx
@@ -1,12 +1,12 @@
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+
+import Spinner from '../../components/Spinner';
 import client from '../../utils/client';
-import CohortList from '../createCohort/components/cohortList';
 import DEFAULTIMG from '../../assets/default.png';
 
 import './style.css';
-import Spinner from '../../components/Spinner';
 
 const ViewCohort = ({ setProfileView }) => {
   const navigate = useNavigate();
@@ -19,10 +19,10 @@ const ViewCohort = ({ setProfileView }) => {
     client
       .get(`/cohort/${cohortId}`)
       .then(res => {
-        setCohort(res);
+        setCohort(res.data.data);
         setIsLoading(false);
       })
-      .catch(err => console.log('fetch /cohort/:id err', err));
+      .catch(err => console.log('[FETCH /cohort/:id]', err));
   }, [cohortId]);
 
   const handleProfileClick = userId => {
@@ -48,21 +48,22 @@ const ViewCohort = ({ setProfileView }) => {
               <h3>Students</h3>
               <ul>
                 {cohort?.users?.map(user => (
-                  <li onClick={() => handleProfileClick(user.id)}>
-                    <div className="enrolment__student-card">
-                      <img
-                        className="enrolment__profile-image"
-                        src={user.profile_image_url || DEFAULTIMG}
-                        alt="Profile"
-                      />
+                  <li
+                    className="view-cohort__student-card"
+                    onClick={() => handleProfileClick(user.id)}
+                  >
+                    <img
+                      className="view-cohort__profile-image"
+                      src={user.profile_image_url || DEFAULTIMG}
+                      alt="Profile"
+                    />
 
-                      <div className="enrolment__user-text">
-                        <span style={{ marginRight: '8px' }}>
-                          {user.first_name}
-                        </span>
-                        <span>{user.last_name}</span>
-                        <p className="enrolment__user-email">{user.email}</p>
-                      </div>
+                    <div className="view-cohort__user-text">
+                      <span style={{ marginRight: '8px' }}>
+                        {user.first_name}
+                      </span>
+                      <span>{user.last_name}</span>
+                      <p className="view-cohort__user-email">{user.email}</p>
                     </div>
                   </li>
                 ))}
