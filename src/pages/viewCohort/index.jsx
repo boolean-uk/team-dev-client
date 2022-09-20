7c3342fa3704daba17fdccf3f1627e70be117ada
Hubert Lemczak <hubertlemczak@gmail.com>
2022-09-20 12:43:33 +0100

    feat: added spinner component

--- src/pages/viewCohort/index.jsx
+++ src/pages/viewCohort/index.jsx
@@ -1,24 +1,76 @@
 import { useEffect } from 'react';
 import { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import client from '../../utils/client';
+import CohortList from '../createCohort/components/cohortList';
+import DEFAULTIMG from '../../assets/default.png';
+
 import './style.css';
+import Spinner from '../../components/Spinner';
 
-const ViewCohort = () => {
-  const cohortId = parseInt(useParams()?.cohortId);
+const ViewCohort = ({ setProfileView }) => {
+  const navigate = useNavigate();
+  const [cohort, setCohort] = useState();
+  const [isLoading, setIsLoading] = useState(true);
 
-  const [cohort, setCohort] = useState({});
+  const cohortId = parseInt(useParams().cohortId);
 
   useEffect(() => {
     client
       .get(`/cohort/${cohortId}`)
-      .then(console.log)
+      .then(res => {
+        setCohort(res);
+        setIsLoading(false);
+      })
       .catch(err => console.log('fetch /cohort/:id err', err));
   }, [cohortId]);
 
+  const handleProfileClick = userId => {
+    setProfileView(userId);
+    navigate('/profile');
+  };
+
   return (
-    <div>
-      <div>hi</div>
+    <div style={{ paddingInline: '10px' }}>
+      <div className="view-cohort-container">
+        {isLoading ? (
+          <Spinner
+            style={{
+              position: 'absolute',
+              right: '50%',
+              top: '50%',
+            }}
+          />
+        ) : (
+          <>
+            <h2>{cohort?.name}</h2>
+            <div className="view-cohort-students">
+              <h3>Students</h3>
+              <ul>
+                {cohort?.users?.map(user => (
+                  <li onClick={() => handleProfileClick(user.id)}>
+                    <div className="enrolment__student-card">
+                      <img
+                        className="enrolment__profile-image"
+                        src={user.profile_image_url || DEFAULTIMG}
+                        alt="Profile"
+                      />
+
+                      <div className="enrolment__user-text">
+                        <span style={{ marginRight: '8px' }}>
+                          {user.first_name}
+                        </span>
+                        <span>{user.last_name}</span>
+                        <p className="enrolment__user-email">{user.email}</p>
+                      </div>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 };
