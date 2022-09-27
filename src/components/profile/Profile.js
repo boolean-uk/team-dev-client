a09e7c98db231553191591df132845110bbf2da4
Noemi <noemi.caggiano@gmail.com>
2022-09-27 16:20:29 +0100

    refactor

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,6 +1,6 @@
 import { Button, Avatar, Link } from '@mui/material';
 import './style.css';
-import { Navigate, useLocation, useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import {useLoggedInUser} from '../../context/LoggedInUser'
 import EditForm from './EditForm';
 import client from '../../utils/client';
