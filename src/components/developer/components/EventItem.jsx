482450d1655d2e2b3f3dabe26e10fd2fbce0383a
miresque <mirkilinc@gmail.com>
2022-10-05 16:25:15 +0100

    feat: import user details from api response

--- src/components/developer/components/EventItem.jsx
+++ src/components/developer/components/EventItem.jsx
@@ -1,27 +1,6 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
-import client from '../../../utils/client'
 
 const EventItem = (devEvent) => {
-    const [receivedBy, setReceivedBy] = useState({})
-    const [createdBy, setCreatedBy] = useState({})
-    const eventLog = devEvent.devEvent
-
-    useEffect(() => {
-        const receivedByUserId = eventLog.receivedById;
-        if (receivedByUserId !== null) {
-            client
-            .get(`/user/${eventLog.receivedById}`)
-            .then(res => {setReceivedBy(res.data.data.user)})
-        }
-        const createdByUserId = eventLog.createdById;
-        if (createdByUserId !== null) {
-            client
-            .get(`/user/${eventLog.createdById}`)
-            .then(res => {setCreatedBy(res.data.data.user)})
-        }
-        // eslint-disable-next-line
-    },[])
+  const eventLog = devEvent.devEvent
 
   return (
     <li className='event-item'>
@@ -32,13 +11,23 @@ const EventItem = (devEvent) => {
             <p className="createdAt-time">{eventLog.createdAt}</p>
         </div>
         <p className="event-content">
-            <span className='event-user-name'>{receivedBy.first_name} {receivedBy.last_name}</span>
+            <span className='event-user-name'>
+                {eventLog.receivedBy?.profile.firstName}
+                {' '}
+                {eventLog.receivedBy?.profile.lastName}
+                { eventLog.receivedBy ? ('(' + eventLog.receivedBy?.role + ')') : null }
+            </span>
             {' '}
             {eventLog.topic}
             {' '}
             {eventLog.content}
             {' '}
-            <span className='event-user-name'>{createdBy.first_name} {createdBy.last_name}</span>
+            <span className='event-user-name'>
+                {eventLog.createdBy?.profile.firstName}
+                {' '}
+                {eventLog.createdBy?.profile.lastName}
+                { eventLog.createdBy ? ('(' + eventLog.createdBy?.role + ')') : null }
+            </span>
         </p>
     </li>
   )
--- src/components/developer/components/EventList.jsx
+++ src/components/developer/components/EventList.jsx
@@ -1,11 +1,9 @@
 import EventItem from './EventItem'
 
 const EventList = (eventLogs) => {
-    // console.log('what is this', eventLogs)
   return (
     <ul className='events-list'>
         { eventLogs.eventLogs.map((devEvent, index) => {
-            // console.log('devEvent',devEvent)
             return <EventItem devEvent={devEvent} key={index} />
           }
         )}
